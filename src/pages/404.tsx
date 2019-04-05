ddb51d817175388437635b829ee586100a44af7a
Spencer Miskoviak <smiskoviak@gmail.com>
2019-04-04 21:34:25 -0700

    Fix 404 a lil bit

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,25 +1,47 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from "react";
+import { graphql, PageRendererProps } from "gatsby";
+import styled from "styled-components";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { ContentContainer } from "../components/content-container";
+import { rhythm } from "../utils/typography";
+import { Query } from "../generated/graphql";
 
-class NotFoundPage extends React.Component {
+const Container = styled.div`
+  margin-top: ${rhythm(3)};
+`;
+
+const Description = styled.p`
+  margin: 0;
+`;
+
+interface Props extends PageRendererProps {
+  data: Query;
+}
+
+class NotFoundPage extends React.Component<Props> {
   render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+    const { data } = this.props;
+    const siteTitle = data.site.siteMetadata.title;
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Container>
+          <ContentContainer>
+            <h3>Not Found 😭😭😭</h3>
+            <Description>
+              You just hit a route that doesn&#39;t exist... the sadness.
+            </Description>
+          </ContentContainer>
+        </Container>
       </Layout>
-    )
+    );
   }
 }
 
-export default NotFoundPage
+export default NotFoundPage;
 
 export const pageQuery = graphql`
   query {
@@ -29,4 +51,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
