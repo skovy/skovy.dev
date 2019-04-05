b020fe1cd8695543b846750de8f27791e02aa4d5
Spencer Miskoviak <smiskoviak@gmail.com>
2019-04-04 21:14:12 -0700

    Include Lato font from google fonts

--- src/utils/typography.tsx
+++ src/utils/typography.tsx
@@ -1,4 +1,4 @@
-import Typography from "typography"
+import Typography from "typography";
 
 const typography = new Typography({
   baseFontSize: 16,
@@ -9,19 +9,23 @@ const typography = new Typography({
       name: "Raleway",
       styles: ["400", "700", "700i"],
     },
+    {
+      name: "Lato",
+      styles: ["400", "700", "700i"],
+    },
   ],
   overrideThemeStyles: ({ rhythm }) => ({
     "h1,h2,h3": {
       lineHeight: rhythm(1.3),
     },
   }),
-})
+});
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
+  typography.injectStyles();
 }
 
-export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
+export default typography;
+export const rhythm = typography.rhythm;
+export const scale = typography.scale;
